35cd60e0f79922a2f81ebe2c287fa8df11769d53
DonBoxGit <donbox@yandex.ru>
2023-06-03 22:30:41 +0300

    Added SET_ALLARM output.

--- include/menuTree.h
+++ include/menuTree.h
@@ -4,26 +4,102 @@
 #include <inttypes.h>
 #include "ledDisplay.h"
 
+/* For blink of setting numbers */
+extern Blink blinkPointsTimer;
+extern Timer checkTime;
+
 enum class Menu : uint8_t {
-  SET_CLOCK = 0,
+  SELECTION_MENU = 0,
+  SET_CLOCK,
   SET_ALLARM,
   SET_BRIGHTNESS
-} menuStatus;
+} menuState;
 
-void menuPrint() {
-    switch (menuStatus) {
-        case Menu::SET_CLOCK:
-            displayTM1637.clear();
-            uint8_t data[4] = { _S, _t, _C, _l };
-            displayTM1637.displayByte(data);
-            break;
+enum class subMenu: uint8_t {
+  SET_HOURS = 0,
+  SET_MINUTES
+} subMenuState;
 
-        case Menu::SET_ALLARM:
-        break;
-
-        case Menu::SET_BRIGHTNESS:
-        break;
+void menuPrint(int8_t value) {
+  switch (menuState) {
+    case Menu::SELECTION_MENU:
+    displayTM1637.point(POINT_OFF);
+      switch (value) {
+        case static_cast<int8_t>(Menu::SET_CLOCK):
+          displayTM1637.displayByte(_C, _L, _C, _empty);
+          break;
+        
+        case static_cast<int8_t>(Menu::SET_ALLARM):
+          displayTM1637.displayByte(_A, _L, _r, _empty);
+          break;
+        
+        case static_cast<int8_t>(Menu::SET_BRIGHTNESS):
+          displayTM1637.displayByte(_B, _r, _t, _empty);
+          break;
       }
+      break;
+
+    case Menu::SET_CLOCK:
+      switch (subMenuState) {
+        case subMenu::SET_HOURS:
+          if (blinkPointsTimer.getStatus()) {
+            displayTM1637.display(0, value / 10);
+            displayTM1637.display(1, value % 10);
+          } else {
+            displayTM1637.displayByte(0, _empty);
+            displayTM1637.displayByte(1, _empty);
+          }
+          displayTM1637.display(2, getTimeDS3231()[2]);
+          displayTM1637.display(3, getTimeDS3231()[3]);
+          break;
+
+        case subMenu::SET_MINUTES:
+          displayTM1637.display(0,getTimeDS3231()[0]);
+          displayTM1637.display(1, getTimeDS3231()[1]);
+          if (blinkPointsTimer.getStatus()) {
+            displayTM1637.display(2, value / 10);
+            displayTM1637.display(3, value % 10);
+          } else {
+            displayTM1637.displayByte(2, _empty);
+            displayTM1637.displayByte(3, _empty);
+          }
+          break;
+      }
+      break;
+
+    case Menu::SET_ALLARM:
+      switch (subMenuState) {
+        case subMenu::SET_HOURS:
+          if (blinkPointsTimer.getStatus()) {
+            displayTM1637.display(0, value / 10);
+            displayTM1637.display(1, value % 10);
+          } else {
+            displayTM1637.displayByte(0, _empty);
+            displayTM1637.displayByte(1, _empty);
+          }
+          displayTM1637.display(2, getAlarm1().minute / 10);
+          displayTM1637.display(3, getAlarm1().minute % 10);
+          break;
+        
+        case subMenu::SET_MINUTES:
+          displayTM1637.display(0, getAlarm1().hour / 10);
+          displayTM1637.display(1, getAlarm1().hour % 10);
+          if (blinkPointsTimer.getStatus()) {
+            displayTM1637.display(2, value / 10);
+            displayTM1637.display(3, value % 10);
+          } else {
+            displayTM1637.displayByte(2, _empty);
+            displayTM1637.displayByte(3, _empty);
+          }
+          break;
+      }
+
+      break;
+
+    case Menu::SET_BRIGHTNESS:
+
+    break;
+    }
 }
 
 #endif /* _MENU_TREE_H_ */
